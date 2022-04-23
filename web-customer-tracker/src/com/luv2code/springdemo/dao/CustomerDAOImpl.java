5c1c03d79442a0aa6475cbe106e1bb9e5271f884
anubhav <anubhav@DESKTOP-AJ38VOP>
2022-04-23 23:47:18 +0530

    2nd update

--- web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -26,13 +26,50 @@ public class CustomerDAOImpl implements CustomerDAO {
 		
 		//create a query
 		Query<Customer> theQuery = 
-				currentSession.createQuery("from Customer", Customer.class);
+				currentSession.createQuery("from Customer order by lastName", Customer.class);
 		
 		//execute query  and get result list
 		List<Customer> customers = theQuery.getResultList(); 
 		
 		//return the results
 		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// save the customer 
+		currentSession.saveOrUpdate(theCustomer);
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// now retrieve/read from database using the primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+
+		return theCustomer;
+
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// delete object with primary key
+		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
+		
+		theQuery.setParameter("customerId",theId);
+		
+		theQuery.executeUpdate();
 	}
 
 }
